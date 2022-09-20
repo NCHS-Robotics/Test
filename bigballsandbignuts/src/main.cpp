1a3b7765eb39b4d123f3427997983f75fe184e59
Alex Montchik <alexander@montchik.net>
2022-09-20 17:13:30 -0500

    9/20 intake & controller drive done, no multitask

--- bigballsandbignuts/src/main.cpp
+++ bigballsandbignuts/src/main.cpp
@@ -7,40 +7,40 @@
 #define INTAKE_MOTOR_PORT 5
 
 void opcontrol() {
-    pros::Motor left_front_wheel(LEFT_FRONT_WHEEL_PORT); 
-    pros::Motor left_back_wheel(LEFT_BACK_WHEEL_PORT);
-    pros::Motor right_front_wheel(RIGHT_FRONT_WHEEL_PORT);
-    pros::Motor right_back_wheel(RIGHT_BACK_WHEEL_PORT);
+    pros::Motor left_front_wheel(LEFT_FRONT_WHEEL_PORT, true); 
+    pros::Motor left_back_wheel(LEFT_BACK_WHEEL_PORT, true);
+    pros::Motor right_front_wheel(RIGHT_FRONT_WHEEL_PORT, true);
+    pros::Motor right_back_wheel(RIGHT_BACK_WHEEL_PORT, true);
 
     pros::Motor intake_motor(INTAKE_MOTOR_PORT);
 
     pros::Controller master (pros::E_CONTROLLER_MASTER);
 
     while (true) {
-        int dtY = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
-        int dtX = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+        int dtY = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int dtX = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
 
-        left_back_wheel.move(dtX - dtY);
-        left_front_wheel.move(dtX - dtY);
-        right_back_wheel.move(dtX + dtY);
-        right_front_wheel.move(dtX + dtY);
+        left_back_wheel.move((dtX + dtY));
+        left_front_wheel.move((dtX + dtY));
+        right_back_wheel.move((dtX - dtY));
+        right_front_wheel.move((dtX - dtY));
 
-        intake_motor.move_velocity(200 * master.get_digital(DIGITAL_L1));
-        intake_motor.move_velocity(-200 * master.get_digital(DIGITAL_L2));
+        intake_motor.move_velocity(-200 * master.get_digital(DIGITAL_L1));
+        intake_motor.move_velocity(200 * master.get_digital(DIGITAL_L2));
         
 
         /**
         while (master.get_digital(DIGITAL_L1) == 1) {
-            intake_motor.move_velocity(200);
+            intake_motor.move_velocity(-200);
         }
         intake_motor.move_velocity(0);
         
 
         while (master.get_digital(DIGITAL_L2) == 1) {
-            intake_motor.move_velocity(-200);
+            intake_motor.move_velocity(200);
         }
         intake_motor.move_velocity(0);
-        */ 
+        */
 
         pros::delay(2);
     }
