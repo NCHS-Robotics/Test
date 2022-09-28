afcd8025e76ac287ed3e530638bb63e901a3e1ed
Alex Montchik <alexander@montchik.net>
2022-09-28 14:29:40 -0500

    9/28 multitasking

--- bigballsandbignuts/src/main.cpp
+++ bigballsandbignuts/src/main.cpp
@@ -6,41 +6,44 @@
 #define RIGHT_BACK_WHEEL_PORT 2
 #define INTAKE_MOTOR_PORT 5
 
+pros::Motor left_front_wheel(LEFT_FRONT_WHEEL_PORT, true); 
+pros::Motor left_back_wheel(LEFT_BACK_WHEEL_PORT, true);
+pros::Motor right_front_wheel(RIGHT_FRONT_WHEEL_PORT, true);
+pros::Motor right_back_wheel(RIGHT_BACK_WHEEL_PORT, true);
+
+pros::Motor intake_motor(INTAKE_MOTOR_PORT);
+
+pros::Controller master (pros::E_CONTROLLER_MASTER);
+
+void driveTask(void* param) {
+    int dtY = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    int dtX = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
+
+    left_back_wheel.move((dtX + dtY));
+    left_front_wheel.move((dtX + dtY));
+    right_back_wheel.move((dtX - dtY));
+    right_front_wheel.move((dtX - dtY));
+}
+
+void intakeIn(void* param) {
+    //intake_motor.move_velocity(-200 * master.get_digital(DIGITAL_L1));
+    intake_motor.move_velocity(200 * master.get_digital(DIGITAL_L2));
+}
+
+void intakeOut(void* param) {
+    intake_motor.move_velocity(-200 * master.get_digital(DIGITAL_L1));
+    //intake_motor.move_velocity(200 * master.get_digital(DIGITAL_L2));
+}
+
+
+
 void opcontrol() {
-    pros::Motor left_front_wheel(LEFT_FRONT_WHEEL_PORT, true); 
-    pros::Motor left_back_wheel(LEFT_BACK_WHEEL_PORT, true);
-    pros::Motor right_front_wheel(RIGHT_FRONT_WHEEL_PORT, true);
-    pros::Motor right_back_wheel(RIGHT_BACK_WHEEL_PORT, true);
-
-    pros::Motor intake_motor(INTAKE_MOTOR_PORT);
-
-    pros::Controller master (pros::E_CONTROLLER_MASTER);
+    
 
     while (true) {
-        int dtY = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-        int dtX = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
-
-        left_back_wheel.move((dtX + dtY));
-        left_front_wheel.move((dtX + dtY));
-        right_back_wheel.move((dtX - dtY));
-        right_front_wheel.move((dtX - dtY));
-
-        intake_motor.move_velocity(-200 * master.get_digital(DIGITAL_L1));
-        intake_motor.move_velocity(200 * master.get_digital(DIGITAL_L2));
-        
-
-        /**
-        while (master.get_digital(DIGITAL_L1) == 1) {
-            intake_motor.move_velocity(-200);
-        }
-        intake_motor.move_velocity(0);
-        
-
-        while (master.get_digital(DIGITAL_L2) == 1) {
-            intake_motor.move_velocity(200);
-        }
-        intake_motor.move_velocity(0);
-        */
+        pros::Task drive(driveTask);
+        pros::Task intakeI(intakeIn);
+        pros::Task intakeO(intakeOut);
 
         pros::delay(2);
     }
