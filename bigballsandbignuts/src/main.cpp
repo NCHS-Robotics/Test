414a67fef1172c08456953b41ec37c9c820a6e90
Alex Montchik <alexander@montchik.net>
2022-10-25 16:03:19 -0500

    10/25 fixed controller code to have drivetrain and intake working simultaneously
    this took like a month lol

--- bigballsandbignuts/src/main.cpp
+++ bigballsandbignuts/src/main.cpp
@@ -15,6 +15,7 @@ pros::Motor intake_motor(INTAKE_MOTOR_PORT);
 
 pros::Controller master (pros::E_CONTROLLER_MASTER);
 
+
 void driveTask(void* param) {
     int dtY = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
     int dtX = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
@@ -25,25 +26,118 @@ void driveTask(void* param) {
     right_front_wheel.move((dtX - dtY));
 }
 
+
 void intakeIn(void* param) {
-    //intake_motor.move_velocity(-200 * master.get_digital(DIGITAL_L1));
-    intake_motor.move_velocity(200 * master.get_digital(DIGITAL_L2));
+    /**
+        while (master.get_digital(DIGITAL_L1) == 1) {
+            intake_motor.move_velocity(200);
+        }
+        intake_motor.move_velocity(0);
+     **/ //doesn't stop
+
+    //intake_motor.move_velocity(200 * master.get_digital(DIGITAL_L1));
+    intake_motor.move_velocity(200 * (-1*(master.get_digital(DIGITAL_L2))));
 }
 
 void intakeOut(void* param) {
-    intake_motor.move_velocity(-200 * master.get_digital(DIGITAL_L1));
-    //intake_motor.move_velocity(200 * master.get_digital(DIGITAL_L2));
+    /**
+     while (master.get_digital(DIGITAL_L2) == 1) {
+            intake_motor.move_velocity(-200);
+        }
+        intake_motor.move_velocity(0);
+    **/ //doesn't stop    
+
+    //intake_motor.move_velocity(-200 * master.get_digital(DIGITAL_L2));
+    //intake_motor.move_velocity(200 * (-1*(master.get_digital(DIGITAL_L2))));
+    intake_motor.move_velocity(200  * master.get_digital(DIGITAL_L2));
 }
 
+void intakeController(void* param) {
+    int y = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+    intake_motor.move(y);
+}
 
+/**
+void intakeTask(void* param) {
+    while (master.get_digital(DIGITAL_L1) == 1) {
+            intake_motor.move_velocity(200);
+        }
+        intake_motor.move_velocity(0);
+    while (master.get_digital(DIGITAL_L2) == 1) {
+            intake_motor.move_velocity(-200);
+        }
+        intake_motor.move_velocity(0);    
+    //intake_motor.move_velocity(200 * master.get_digital(DIGITAL_L1));
+    //intake_motor.move_velocity(-200 * master.get_digital(DIGITAL_L2));
+}
+**/
+/**
+void leftBackDrive(void* param) {
+    int dtY = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    int dtX = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
+
+    left_back_wheel.move((dtX + dtY));
+}
+
+void leftFrontDrive(void* param) {
+    int dtY = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    int dtX = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
+
+    left_front_wheel.move((dtX + dtY));
+}
+
+void rightBackDrive(void* param) {
+    int dtY = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    int dtX = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
+
+    right_back_wheel.move((dtX - dtY));
+}
+
+void rightFrontDrive(void* param) {
+    int dtY = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    int dtX = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
+
+    right_front_wheel.move((dtX - dtY));
+}
+**/
 
 void opcontrol() {
-    
-
     while (true) {
+        /**
+        //drive code
+        int dtY = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int dtX = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
+
+        left_back_wheel.move((dtX + dtY));
+        left_front_wheel.move((dtX + dtY));
+        right_back_wheel.move((dtX - dtY));
+        right_front_wheel.move((dtX - dtY)); 
+        **/
+
+
+        /**
+        //intake code
+        while (master.get_digital(DIGITAL_L1) == 1) {
+            intake_motor.move_velocity(200);
+        }
+        intake_motor.move_velocity(0);
+        while (master.get_digital(DIGITAL_L2) == 1) {
+            intake_motor.move_velocity(-200);
+        }
+        intake_motor.move_velocity(0);  
+        **/
+          
         pros::Task drive(driveTask);
-        pros::Task intakeI(intakeIn);
-        pros::Task intakeO(intakeOut);
+
+        //pros::Task intake(intakeTask);
+        //pros::Task intakeI(intakeIn);
+        //pros::Task intakeO(intakeOut);
+        pros::Task intakeC(intakeController);
+
+        //pros::Task leftBDrive(leftBackDrive);
+        //pros::Task leftFDrive(leftFrontDrive);
+        //pros::Task rightBDrive(rightBackDrive);
+        //pros::Task rightFDrive(rightFrontDrive);
 
         pros::delay(2);
     }
